459af4bbfd69b4a53f7ab78946fc536cbcc915d5
matias-francisco-deo <matiasfd.deo@gmail.com>
2026-08-26 15:43:39 -0300

    inicialización de spring boot

--- src/test/java/TestSimple.java
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-public class TestSimple {
-    @Test
-    void addition() {
-        assertEquals(4, 2 + 2);
-    }
-}
