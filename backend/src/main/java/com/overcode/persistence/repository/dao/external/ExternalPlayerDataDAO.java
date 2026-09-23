a193347192c77a18b98aca6bb59eb3d43ab5e4f7
ivanros02 <ivanrosendo1102@gmail.com>
2026-09-23 11:06:19 -0300

    Actualiza ExternalPlayerDataDAO

--- backend/src/main/java/com/overcode/persistence/repository/dao/external/ExternalPlayerDataDAO.java
+++ backend/src/main/java/com/overcode/persistence/repository/dao/external/ExternalPlayerDataDAO.java
@@ -4,9 +4,10 @@ import com.overcode.model.Player;
 import com.overcode.persistence.dto.external.PlayerDraftDTO;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ExternalPlayerDataDAO {
-    Player getDatosDeJugador(PlayerDraftDTO playerDraftDTO);
+    Optional<Player> getDatosDeJugador(PlayerDraftDTO playerDraftDTO);
     List<Player> getDatosJugadores(List<PlayerDraftDTO> playerDraftDTOS);
 
 }
