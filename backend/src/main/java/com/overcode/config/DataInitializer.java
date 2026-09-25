c73f9245951d241f3414aa769e265aec9e5a0efb
matias-francisco-deo <matiasfd.deo@gmail.com>
2026-09-25 17:43:32 -0300

    fix: fixes varios

--- backend/src/main/java/com/overcode/config/DataInitializer.java
+++ backend/src/main/java/com/overcode/config/DataInitializer.java
@@ -1,44 +1,21 @@
 package com.overcode.config;
 
-import com.overcode.persistence.repository.interfaces.UserRepository;
-import com.overcode.service.interfaces.PlayerService;
 import com.overcode.service.interfaces.UserService;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @Component
 public class DataInitializer {
     private final UserService userService;
-    private final PlayerService playerService;
 
     public DataInitializer(
-            UserService userService, PlayerService playerService
-    ) {
-        this.userService = userService;
-        this.playerService = playerService;
-    }
+            UserService userService
+    ) {this.userService = userService;}
 
     @PostConstruct
     @Transactional
     public void initialize() {
-
         userService.crearSuperusuario();
-//
-//        List<String> names = List.of(
-//            "Lionel Messi",
-//            "Kylian Mbappé",
-//            "Erling Haaland",
-//            "Vinicius Júnior",
-//            "Rodri",
-//            "Jude Bellingham"
-//        );
-
-//        for (String name : names) {
-//            PlayerRecord player = playerDAO.save(new PlayerRecord(name, 100, 100));
-//            // positionRepository.save(new PositionRecord(superUser.getId(), player.getId(), 100));
-//        }
     }
 }
